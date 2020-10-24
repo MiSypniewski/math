43d732d3ce07563ce6449001cb61b417c82e5d41
Michał Sypniewski <mi.sypniewski@gmail.com>
2020-10-24 18:28:52 +0200

    Updates

---
+++ precache-manifest.d4ffacc32b02d91cf858558fdf8ef2b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082dc2c2ac0bbaa84667f252c2c0e12c",
+    "url": "/math/index.html"
+  },
+  {
+    "revision": "ea9d673491d1074ae0c5",
+    "url": "/math/static/js/2.98e4555f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/math/static/js/2.98e4555f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbd0522d84d46cc67021",
+    "url": "/math/static/js/main.f265aff7.chunk.js"
+  },
+  {
+    "revision": "96f44b2d079243e66007",
+    "url": "/math/static/js/runtime-main.3f916fd3.js"
+  }
+]);
